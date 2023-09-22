82759f2916c8379597a7a4ea01384bd6d005dca1
HiepLC <hieplechi21042001@gmail.com>
2023-09-22 17:35:28 +0700

    update api

--- api/controllers/statisticController.js
+++ api/controllers/statisticController.js
@@ -2,19 +2,50 @@ const db = require("../databases/db");
 
 const shoeStatisticController = {
   getStatisticByWeekly(req, res) {
+//     const { fromday, today } = req.query;
+
+//     const query = `
+//     SELECT
+//   DATE(order_date) AS day,
+//   SUM(total_price) AS revenue
+// FROM
+//   shoe_order
+// WHERE
+//   order_date BETWEEN '${fromday}' AND '${today}'
+// GROUP BY
+//   DATE(order_date);
+//   `;
+
+//     db.query(query, (err, results) => {
+//       if (err) {
+//         console.error("Error executing query:", err);
+//         res.status(500).json({ error: "Internal Server Error" });
+//         return;
+//       }
+
+//       res.json(results);
+    //     });
     const { fromday, today } = req.query;
 
     const query = `
-    SELECT
-  DATE(order_date) AS day,
-  SUM(total_price) AS revenue
-FROM
-  shoe_order
-WHERE
-  order_date BETWEEN '${fromday}' AND '${today}'
-GROUP BY
-  DATE(order_date);
-  `;
+  SELECT
+    all_dates.day,
+    COALESCE(SUM(shoe_order.total_price), 0) AS revenue
+  FROM
+    (
+      SELECT DATE('${fromday}' + INTERVAL (a.a + (10 * b.a) + (100 * c.a)) DAY) AS day
+      FROM
+        (SELECT 0 AS a UNION ALL SELECT 1 UNION ALL SELECT 2 UNION ALL SELECT 3 UNION ALL SELECT 4 UNION ALL SELECT 5 UNION ALL SELECT 6 UNION ALL SELECT 7 UNION ALL SELECT 8 UNION ALL SELECT 9) AS a
+        CROSS JOIN (SELECT 0 AS a UNION ALL SELECT 1 UNION ALL SELECT 2 UNION ALL SELECT 3 UNION ALL SELECT 4 UNION ALL SELECT 5 UNION ALL SELECT 6 UNION ALL SELECT 7 UNION ALL SELECT 8 UNION ALL SELECT 9) AS b
+        CROSS JOIN (SELECT 0 AS a UNION ALL SELECT 1 UNION ALL SELECT 2 UNION ALL SELECT 3 UNION ALL SELECT 4 UNION ALL SELECT 5 UNION ALL SELECT 6 UNION ALL SELECT 7 UNION ALL SELECT 8 UNION ALL SELECT 9) AS c
+    ) AS all_dates
+  LEFT JOIN
+    shoe_order ON all_dates.day = DATE(shoe_order.order_date)
+  WHERE
+    all_dates.day BETWEEN '${fromday}' AND '${today}'
+  GROUP BY
+    all_dates.day;
+`;
 
     db.query(query, (err, results) => {
       if (err) {
@@ -24,26 +55,48 @@ GROUP BY
       }
 
       res.json(results);
-    });
+    });0
   },
 
   getStatisticByMonthly(req, res) {
-    const { month, year } = req.query;
+  //   const { month, year } = req.query;
+
+  //   const query = `
+  //   SELECT
+  //     DATE(order_date) AS date,
+  //     SUM(total_price) AS revenue
+  //   FROM
+  //     shoe_order
+  //   WHERE
+  //     MONTH(order_date) = ${month}
+  //     AND YEAR(order_date) = ${year}
+  //   GROUP BY
+  //     DATE(order_date)
+  //   ORDER BY
+  //     order_date ASC;
+  // `;
+
+  //   db.query(query, (err, results) => {
+  //     if (err) {
+  //       console.error("Error executing query:", err);
+  //       res.status(500).json({ error: "Internal Server Error" });
+  //       return;
+  //     }
+  //     res.status(200).json(results);
+    //   });
+    const { fromMonth, toMonth } = req.query;
 
     const query = `
-    SELECT
-      DATE(order_date) AS date,
-      SUM(total_price) AS revenue
-    FROM
-      shoe_order
-    WHERE
-      MONTH(order_date) = ${month}
-      AND YEAR(order_date) = ${year}
-    GROUP BY
-      DATE(order_date)
-    ORDER BY
-      order_date ASC;
-  `;
+  SELECT
+    DATE_FORMAT(order_date, '%Y-%m') AS month,
+    COALESCE(SUM(total_price), 0) AS revenue
+  FROM
+    shoe_order
+  WHERE
+    DATE_FORMAT(order_date, '%Y-%m') BETWEEN '${fromMonth}' AND '${toMonth}'
+  GROUP BY
+    DATE_FORMAT(order_date, '%Y-%m');
+`;
 
     db.query(query, (err, results) => {
       if (err) {
@@ -51,7 +104,8 @@ GROUP BY
         res.status(500).json({ error: "Internal Server Error" });
         return;
       }
-      res.status(200).json(results);
+
+      res.json(results);
     });
   },
 };
--- api/routes/statisticRoute.js
+++ api/routes/statisticRoute.js
@@ -4,8 +4,8 @@ const router = express.Router();
 const shoeStatisticController = require("../controllers/statisticController");
 
 // Định nghĩa route GET /shoeStatistics
-router.get("/statistics/monthly", shoeStatisticController.getStatisticByMonthly);
+router.get("/statistics/month", shoeStatisticController.getStatisticByMonthly);
 
-router.get("/statistics/weekly", shoeStatisticController.getStatisticByWeekly);
+router.get("/statistics/day", shoeStatisticController.getStatisticByWeekly);
 
 module.exports = router;
