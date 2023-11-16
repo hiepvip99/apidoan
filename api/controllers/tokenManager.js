34ec27f30b5357732f8df6d99bf54cb1e7bc0bed
HiepLC <hieplechi21042001@gmail.com>
2023-11-16 12:35:54 +0700

    chua test code

--- api/controllers/tokenManager.js
+++ api/controllers/tokenManager.js
@@ -30,3 +30,44 @@ module.exports = {
     getResetToken,
     deleteResetToken,
 };
+
+
+/* const randomstring = require('randomstring');
+let resetTokens = {};
+
+async function generateToken(email) {
+    // Generate a random 6-digit token
+    const resetToken = randomstring.generate({ length: 6, charset: 'numeric' });
+
+    // Store the token in the resetTokens object with a 5-minute expiration
+    await updateResetTokens(email, {
+        token: resetToken,
+        expiration: Date.now() + 30 * 60 * 1000, // 10 minutes in milliseconds
+    });
+
+    return resetToken;
+}
+
+async function getResetToken(email) {
+    return resetTokens[email];
+}
+
+async function deleteResetToken(email) {
+    delete resetTokens[email];
+}
+
+async function updateResetTokens(email, data) {
+    return new Promise((resolve) => {
+        setImmediate(() => {
+            resetTokens[email] = data;
+            resolve();
+        });
+    });
+}
+
+module.exports = {
+    generateToken,
+    getResetToken,
+    deleteResetToken,
+};
+ */
